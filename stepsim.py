efd3846dd6aaa59f0736b390ab39e07b3c54cc27
Florian Berger <fberger@florian-berger.de>
2011-02-05 15:18:00 +0100

    Initial commit.

---
+++ stepsim.py
@@ -0,0 +1,21 @@
+"""stepsim - Python Step-based Simulation Package
+
+   Copyright 2011 Florian Berger <fberger@florian-berger.de>
+"""
+
+# This file is part of stepsim.
+#
+# stepsim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# stepsim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with stepsim.  If not, see <http://www.gnu.org/licenses/>.
+
+# Work started on 05 Feb 2011.
